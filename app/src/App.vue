d7386356a7c6133a7c4ccf19d7cc4e8b2d37d041
Muhammad Arslan <83068535+arslanarif786@users.noreply.github.com>
2023-12-10 20:35:01 +0500

    watcher added

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 // import GrandParent from './components/GrandParent.vue'
 import ComputedConcept from "./components/ComputedConcept.vue"
+import WatcherConcept from "./components/WatcherConcept.vue"
 </script>
 
 <template>
   <ComputedConcept />
+  <WatcherConcept />
   <!-- <GrandParent /> -->
 </template>
 

b021a6b88cf54d29251e56f69923a452cf3f9d94
Muhammad Arslan <83068535+arslanarif786@users.noreply.github.com>
2023-12-11 22:31:20 +0500

    Data Binding and watcher concepts done

---
+++ app/src/components/DataBinding.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>Data binding in this component</div>
+
+    <input placeholder="Enter 1st varibale" :value="variableA" @input="changeVaraibleA($event)"/><br>
+
+    <input placeholder="Enter 2nd varibale" v-model="variableB" /><br>
+    <input type="checkbox" placeholder="Enter 3rd varibale" v-model="variableC"/><br>
+    <input placeholder="Enter 4th varibale"/><br>
+    <input placeholder="Enter 5th varibale"/><br>
+</template>
+
+
+<script setup>
+// single way data binding
+import { ref } from 'vue';
+
+const variableA = ref('')
+const variableB = ref(null)
+const variableC = ref(0)
+const variableD = ref(0)
+const variableE = ref(0)
+
+const changeVaraibleA = (typedInput) => {
+    console.log('--->', typedInput.data)
+    variableA.value = typedInput.data
+}
+
+// two way data binding
+</script>
--- app/src/components/WatcherConcept.vue
+++ app/src/components/WatcherConcept.vue
@@ -2,24 +2,120 @@
 import { ref, watch } from 'vue'
 
 const toggle = ref(false)
-const name = ref('ABC')
+const person = ref({
+  name: 'ABC',
+  age: '20'
+})
+
+const arrayOfObjects = ref([
+    {
+      name: 'ABC',
+      age: '20'
+    },
+    {
+      name: 'ABC',
+      age: '20'
+    },
+  ])
+
+const arrayOfArrays = ref([
+  [
+    {
+      name: 'ABC',
+      age: '20'
+    },
+    {
+      name: 'ABC',
+      age: '20'
+    },
+  ],
+  [
+    {
+      name: 'ABC',
+      age: '20'
+    },
+    {
+      name: 'ABC',
+      age: '20'
+    },
+  ],
+  [
+    {
+      name: 'ABC',
+      age: '20'
+    },
+    {
+      name: 'ABC',
+      age: '20'
+    },
+  ]
+])
+
+// watcher ----> it looks to states everytime even if we change its data or not
 
 watch(toggle, (newVal) => {
   console.log(`watcher running ----- toggle new value => ${newVal}`)
   if(newVal) {
-    name.value = 'XYZ'
+    person.value.name = 'XYZ'
   } else {
-    name.value = 'ABC'
+    person.value.name = 'ABC'
   }
-})
+},
+// { immediate: true },   // purpose to use this to get sudden change whenever you face lazy changing behavior in your watcher
+// { deep: true }         // purpose to handle and play with nested level of data structures
+)
+
+// watch([email, phoneNumber, first_name, last_name],
+//   ([newEmail, newPhNumber, newFirstName, newLastName]) => {
+//     if(newFirstName) {
+//       changeFName.value = true
+//     }
+//     if(newLastName) {
+//       changeLName.value = true
+//     }
+//     if(newEmail) {
+//       changeEmail.value = true
+//     }
+//     if(newPhNumber) {
+//       changePhNumber.value = true
+//     }
+//     checkEmail.value = validateEmail(newEmail)
+//     checkPhoneNumber.value = (newPhNumber.toString().length >= 9 && /^[0-9+-]+$/.test(newPhNumber)) ? true : false;
+//     checkFirstName.value = newFirstName.length > 0 ? true : false
+//     checkLastName.value = newLastName.length > 0 ? true : false
+// }, { immediate: true })
+
+
+
+// const props = defineProps({
+//   isOpen: Boolean,
+// })
+
+// const newService = ref({
+//   name: '',
+//   price: '',
+//   picture: ''
+// })
+
+// watch(props, (newVal) => {
+//   if(!newVal.isOpen) {
+//     newService.value.name = ''
+//     newService.value.price = ''
+//     newService.value.picture = ''
+//   }
+// })
 
 </script>
 
 <template>
   <div>
-    <button @click="toggle = !toggle" style="background: green; color: white;">
+
+    <button
+      @click="toggle = !toggle"
+      style="background: green; color: white;">
       Submit
     </button>
-    <p> name is: {{ name }} </p>
+
+    <p> name is: {{ person }} </p>
   </div>
 </template>

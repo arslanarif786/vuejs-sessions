79d6abc3f39eba97187ca543028226b005974cd8
Muhammad Arslan <83068535+arslanarif786@users.noreply.github.com>
2023-12-11 22:32:08 +0500

    Data Binding and watcher concepts done

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,11 @@
 <script setup>
-import ComputedFunction from './components/ComputedFunction.vue';
+// import ComputedFunction from './components/ComputedFunction.vue';
 //import GrandParent from './components/GrandParent.vue'
 //import ComputedConcept from "./components/ComputedConcept.vue"
-//import WatcherConcept from "./components/WatcherConcept.vue"
+// import WatcherConcept from "./components/WatcherConcept.vue"
+import DataBinding from "./components/DataBinding.vue"
 </script>
 
 <template>
- <!-- <ComputedConcept /> -->
- <!-- <WatcherConcept /> -->
-  <!-- <GrandParent /> -->
-  <ComputedFunction />
+ <DataBinding />
 </template>
-
-<style scoped>
-
-</style>

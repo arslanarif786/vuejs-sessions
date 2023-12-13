a2cac6ba3701d44ce2d9cf2db1f95e0bae901a6e
Muhammad Arslan <83068535+arslanarif786@users.noreply.github.com>
2023-12-13 20:46:34 +0500

    slots and composables added

--- app/src/components/DataBindingTask.vue
+++ app/src/components/DataBindingTask.vue
@@ -24,30 +24,8 @@
         <input type="radio" id="field7" :value="field7" @input="updateField7"><br>
     </div>
 </template>
+
 <script setup>
-    import { ref } from 'vue'
-
-    const field1 = ref('')
-    const field2 = ref('')
-    const field3 = ref(0)
-    const field4 = ref('')
-    const field5 = ref('')
-    const field6 = ref(null)
-    const field7 = ref(null)
-
-    const updateField1 = (event) => {
-        console.log('field1 --->', event.target.value)
-        field1.value = event.target.value
-    }
-
-    const updateField2 = (typedInput) => {
-        console.log('field2 --->', typedInput.data)
-        field2.value = typedInput.data
-    }
-
-    const updateField7 = (event) => {
-        console.log('field7 --->', event.target.value)
-        field7.value = event.target.value
-    }
-
+    import useSameFunctionality from '../composables/useSameFunctionality'
+    const { field1, field2, field3, field4, field5,field6, field7, updateField1, updateField2 } = useSameFunctionality()
 </script>
---
+++ app/src/components/StyledButton.vue
@@ -0,0 +1,6 @@
+<template>
+    <button style="background-color: red;color: white; border-radius: 6px;" @click="$emit('button-clicked')">
+        <slot></slot>
+    </button>
+    </template>
+    // emit concept here in the child file
